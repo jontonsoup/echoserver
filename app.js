db2fa2924daf4db899ee7e5d7d0e30a4619713be
Jonathan Friedman <friedmanj98@gmail.com>
2013-12-24 22:23:45 -0500

    FEATURE: Added server

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var express = require('express');
 var app = express();
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 
 app.get('/', function(req, res) {
   res.type('text/plain');
-  var a = "hey";
-  res.json(a);
+  redis.get('text', function(err, value) {
+     res.json({"value": value});
+    res.json(value);
+  });
+});
+
+
+app.post('/set/:text', function(req, res) {
+  res.type('text/plain');
+  if(!req.params.text) {
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }
+  redis.set('text', req.params.text);
+  res.json({"value": req.params.text});
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
