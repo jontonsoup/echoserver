ed2986b79e4639edd619ee5931218fc3917d8956
Jonathan Friedman <friedmanj98@gmail.com>
2013-12-24 22:31:14 -0500

    Cleanup

--- app.js
+++ app.js
@@ -5,18 +5,13 @@ var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 app.get('/', function(req, res) {
   res.type('text/plain');
   redis.get('text', function(err, value) {
-     res.json({"value": value});
-    res.json(value);
+    res.json({"value": value});
   });
 });
 
 
 app.post('/set/:text', function(req, res) {
   res.type('text/plain');
-  if(!req.params.text) {
-    res.statusCode = 404;
-    return res.send('Error 404: No quote found');
-  }
   redis.set('text', req.params.text);
   res.json({"value": req.params.text});
 });
